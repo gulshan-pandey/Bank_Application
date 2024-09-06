755b1a4d62c8a2d9535c4efc7cd78a2c99b727a3
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-09-07 02:57:23 +0530

    Add files via upload

---
+++ Bank_application/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws SQLException {
+		DBOperations db = new DBOperations();
+
+
+			Operation operation = new Operation();
+			operation.bankStart();
+
+	}
+}
